b7ccc99aa398a5af9f3de1a47766a6f404dab368
jim leach <yogajim@gmail.com>
2015-06-15 15:54:35 -0400

    Adding responsive

--- js/templates.js
+++ js/templates.js
@@ -2,7 +2,7 @@ this["template"] = this["template"] || {};
 this["template"]["timelines"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "\n\n <div class=\"tlinfo\">\n\n        <div class=\"icons\">\n        <div class=\"dates\">"
+  return "\n<div class=\"outer\">\n <div class=\"tlinfo\">\n\n        <div class=\"icons\">\n        <div class=\"dates\">"
     + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
     + "</div>\n     <div class=\"times\">"
     + alias3(((helper = (helper = helpers.times || (depth0 != null ? depth0.times : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"times","hash":{},"data":data}) : helper)))
@@ -12,7 +12,7 @@ this["template"]["timelines"] = Handlebars.template({"1":function(depth0,helpers
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h4>\n        <p>"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "</p>\n       </article>\n    </section>\n      </li>\n\n\n";
+    + "</p>\n       </article>\n    </section>\n      </li>\n</div>\n\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper;
 
